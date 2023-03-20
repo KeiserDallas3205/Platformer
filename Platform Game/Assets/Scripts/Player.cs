8e528a5c081b364d916f1d0e79415b6d7bf1dcfa
Reid Trisler <reidtrisler@yahoo.com>
2023-03-19 23:06:42 -0500

    Added in basic movement

---
+++ Platform Game/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  private CharacterController2D charCon;
+  private float move;
+  private bool jump = false;
+  // Start is called before the first frame update
+  void Start() {
+    charCon = GetComponent<CharacterController2D>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    move = 0;
+    move += (Input.GetKey(KeyCode.A) ? -1 : 0);
+    move += (Input.GetKey(KeyCode.D) ? +1 : 0);
+    jump = (Input.GetKey(KeyCode.Space) ? true : false);
+  }
+
+  private void FixedUpdate() {
+    charCon.Move(move, false, jump);
+  }
+}
